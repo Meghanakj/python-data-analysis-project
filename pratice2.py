52a1a99b5641d1837ccd3158712cf2d9dc8920b9
meghkj <meghanakj89272@gmail.com>
2025-04-27 21:29:09 +0530

    spc_new_cluster_setup

---
+++ pratice2.py
@@ -0,0 +1,14 @@
+ls=[1,34,55,67,34,1,2,2]
+p1=list(set(ls))
+print(p1)
+ls1=[i-1 for i in ls]
+print(ls)
+print(ls1)
+items = [1, 2, 3, 4, 5]
+
+for item in items:
+    pass  # We'll process items later
+
+print("Loop completed.")
+z2=[i for i in range(30) if i%2==0]
+print(z2)
